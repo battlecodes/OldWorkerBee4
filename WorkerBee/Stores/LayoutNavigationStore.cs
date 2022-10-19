7e43847c334d61faaa486e5e67856e5d0d5a7e97
battlecodes <calvin.algos@gmail.com>
2022-10-18 23:07:18 -0400

    Adds the NavigationBar

---
+++ WorkerBee/Stores/LayoutNavigationStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkerBee.ViewModels;
+
+namespace WorkerBee.Stores
+{
+    public class LayoutNavigationStore
+    {
+    }
+}
